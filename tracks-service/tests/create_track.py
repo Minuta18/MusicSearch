264defaf164590dfac74079ed9a39a33c81bc5bb
Minuta18 <ig.samsonov10@yandex.ru>
2024-04-26 06:49:43 +0300

    Making tests

---
+++ tracks-service/tests/create_track.py
@@ -0,0 +1,43 @@
+import pytest
+import httpx
+import main
+from tests import conftest
+
+client = httpx.AsyncClient(
+    app=main.fastapi_app, 
+    base_url=f'http://127.0.0.1:6002{main.app.PREFIX}/{main.app.SERVICE_NAME}'
+)
+
+@pytest.mark.asyncio
+async def create_track_test():
+    responce = await client.post('/create', json={
+        'title': 'Created track',
+        'description': 'Created track description',
+        'author_id': 1,
+        'origin_url': 'https://this.is.link/something/okay/nu_ok?ok=true',
+    })
+
+    assert responce.status_code == 201
+
+    responce_json = responce.json()
+    
+    assert responce_json['error'] == False
+    assert responce_json['track']['id'] is not None
+    assert responce_json['track']['name'] == 'Created track'
+    assert responce_json['track']['description'] == 'Created track description'
+    assert responce_json['track']['download_link'] is None
+    assert responce_json['track']['length'] is None
+    assert responce_json['track']['origin_url'] == \
+        'https://this.is.link/something/okay/nu_ok?ok=true'
+    assert responce_json['track']['short_origin_url'] == \
+        'is.link/something/okay/nu_ok'
+    
+    responce2 = await client.post('/create', json={
+        'title': 'Create Track',
+        'description': 'Another description',
+        'author_id': 1,
+        'origin_url': 'https://this.is.also.link',
+    })
+    
+    assert responce2.status_code == 409
+    
